3c0ac3a36acc836f0171b822c64fa92102cf7b00
Silvonei <silvoneildr@gmail.com>
2016-12-02 16:57:41 -0200

    implementando controller cad_disciplinasCtrl

--- Aluno10/www/app/controllers/cad_disciplinasCtrl.js
+++ Aluno10/www/app/controllers/cad_disciplinasCtrl.js
@@ -1,5 +1,27 @@
 angular.module('app.cad_disciplinas', [])
-.controller('cad_disciplinasCtrl', function($scope, $state){
+.controller('cad_disciplinasCtrl', function($scope, $state, daoFactory, msgFactory){
+
+    $scope.disciplinas = daoFactory.getDisciplinas();
+    $scope.turmas = daoFactory.getTurmas();
     
+    $scope.saveRecord = function(Disciplina){
+        $scope.disciplinas.save(Disciplina);
+        $scope.disciplinas.post();
+        $state.go('layout.disciplinas');
+    };
+    
+    $scope.deleteRecord = function(disciplina){
+        msgFactory.confirm('Deseja excluir a disciplina?').then(function(res) {
+            if (!res) return;
+            $scope.disciplinas.delete(disciplina);
+            $scope.disciplinas.post();
+        })
+    };
+    
+    $scope.editRecord = function(disciplina){
+        $scope.disciplina =  OjsUtils.cloneObject(disciplina);
+        $scope.inserting = false;
+        $state.go('layout.cad_disciplinas',{id: disciplina.id});
+    };  
     
 })
--- Aluno10/www/app/controllers/disciplinasCtrl.js
+++ Aluno10/www/app/controllers/disciplinasCtrl.js
@@ -1,39 +1,16 @@
 angular.module('app.disciplinas.lista', [])
-.controller('disciplinasCtrl', function($scope, $ionicModal, $ionicPopover, $state, daoFactory, msgFactory, popupFactory) {
+.controller('disciplinasCtrl', function($scope, $ionicPopover, $state, popupFactory, daoFactory) {
     
     $scope.disciplinas = daoFactory.getDisciplinas();
     $scope.turmas = daoFactory.getTurmas();
 
-    popupFactory.startPopup('./app/views/popup_disciplinas.html', $scope);
-
-    $scope.openPopup = function(event){
-        // $scope.disciplinas = OjsUtils.cloneObject(disciplina);
-        // $scope.inserting = false;
-        $scope.popover.show(event);
-    };   
-
     $scope.addRecord = function() {
         $state.go('layout.cad_disciplinas');
     };
-    
-    $scope.saveRecord = function(Disciplina){
-        $scope.disciplinas.save(Disciplina);
-        $scope.disciplinas.post();
-        $state.go('layout.disciplinas');
-    };
-    
-    $scope.deleteRecord = function(disciplina){
-        msgFactory.confirm('Deseja excluir a disciplina?').then(function(res) {
-            if (!res) return;
-            $scope.disciplinas.delete(disciplina);
-            $scope.disciplinas.post();
-        })
-    };
-    
-    $scope.editRecord = function(disciplina){
-        $scope.disciplina =  OjsUtils.cloneObject(disciplina);
-        $scope.inserting = false;
-        $state.go('layout.cad_disciplinas',{id: disciplina.id});
-    };
 
+    popupFactory.startPopup('./app/views/popup_disciplinas.html', $scope);
+
+    $scope.openPopup = function(event){
+        $scope.popover.show(event);
+    };   
 });
