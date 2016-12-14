3cb2b2c487b5d4639638b112d9bd6d665b4c427e
Silvonei <silvoneildr@gmail.com>
2016-12-14 17:46:33 -0200

    Cadastrar Frequencia

--- Aluno10/www/app/controllers/frequenciasCtrl.js
+++ Aluno10/www/app/controllers/frequenciasCtrl.js
@@ -1,5 +1,46 @@
 angular.module('app.frequencias', [])
-.controller('frequenciasCtrl', function($scope){
+.controller('frequenciasCtrl', function($scope, $ionicModal, daoFactory, msgFactory){
+    
+    $ionicModal.fromTemplateUrl('app/views/cad_frequencias.html',{
+        scope: $scope
+    }).then(function(modal){
+        $scope.modal = modal;
+    });
+        
+    $scope.$on('$destroy', function() {
+        $scope.modal.remove();
+    });
+    
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+    };
 
+    $scope.addRecord = function() {
+        $scope.frequencia = {};
+        $scope.inserting = true;
+        $scope.modal.show();
+    };
+
+    $scope.frequencias = daoFactory.getFrequencias();
+
+    $scope.saveRecord = function(){
+        $scope.frequencias.save($scope.frequencia);
+        $scope.frequencias.post();
+        $scope.closeModal();
+    };
+    
+    $scope.deleteRecord = function(frequencia){
+        msgFactory.confirm('Deseja excluir a frequencia?').then(function(res) {
+            if (!res) return;
+            $scope.frequencias.delete(frequencia);
+            $scope.frequencias.post();
+        })
+    };
+    
+    $scope.editRecord = function(frequencia){
+        $scope.frequencia =  OjsUtils.cloneObject(frequencia);
+        $scope.inserting = false;
+        $scope.modal.show();
+    };
 
 })
